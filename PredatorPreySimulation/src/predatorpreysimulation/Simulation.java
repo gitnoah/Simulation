4aca5e230fb5ec55c05327faca728d22c9c89c99
noah <noah@penners.net>
2018-09-27 09:31:37 -0500

    Initiating comment

--- PredatorPreySimulation/src/predatorpreysimulation/Simulation.java
+++ PredatorPreySimulation/src/predatorpreysimulation/Simulation.java
@@ -1,10 +1,16 @@
 package predatorpreysimulation;
+/**
+ * Predator Prey Simulation
+ * September 27, 2018
+ * Noah, Thomas, Ian
+ * @author pennerno
+ *
+ */
 
 public class Simulation {
 
 	public static void main(String[] args) {
-		//fix
-		//fix2
+		
 
 	}
 
