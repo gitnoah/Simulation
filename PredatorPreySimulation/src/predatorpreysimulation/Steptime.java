6cd8dcc583163e5b40b3ce402d8169e223eb9522
xFlyyer <pajamacruzer@gmail.com>
2018-10-09 09:51:26 -0500

    Timer almost works. Just trying to fix a bug that causes it to only go twice.

--- PredatorPreySimulation/src/predatorpreysimulation/Steptime.java
+++ PredatorPreySimulation/src/predatorpreysimulation/Steptime.java
@@ -1,55 +1,41 @@
 package predatorpreysimulation;
 
+
 import java.util.Timer;
 import java.util.TimerTask;
-
-import javax.swing.JTextField;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
+import java.util.Scanner;
 
 
 public class Steptime {
 	
 	static int interval;
 	static Timer timer;
-	JTextField text;
-	JFrame frame;
-	JLabel label;
-	JPanel panel;
+	
 	
 	public Steptime() {
-		int delay = 1000;
-	    int period = 1000;
-	    String input;
-		
-	    frame = new JFrame();
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(150, 100);
-		frame.setLocationRelativeTo(null);
 	    
-	    //add input eventually
-		text = new JTextField(20);
-		text.addActionListener(null);
-		input = text.getText();
-		timer = new Timer();
-		interval = Integer.parseInt(input);
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("input seconds: ");
+		String secs = input.nextLine();
+		int delay = 500;
+	    int period = 500;
+	    timer = new Timer();
+		interval = Integer.parseInt(secs);
+		System.out.println(secs);
+		
 	    
 		
 		timer.scheduleAtFixedRate(new TimerTask() {
 
 			@Override
 			public void run() {
-				
-				
+				System.out.print(setInterval());
 			}
 			
 		}, delay, period);
 		
-		label.add(text);
-		panel.add(label);
-		frame.setContentPane(panel);
-		frame.setVisible(true);
 		
 	}
 	
