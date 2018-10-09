7ff8558f408206c3713595466bce1cb09efa2f30
Ian <loeweni@wcmiller.ca>
2018-10-09 09:55:40 -0500

    frame change

--- PredatorPreySimulation/src/predatorpreysimulation/SimulationGUI.java
+++ PredatorPreySimulation/src/predatorpreysimulation/SimulationGUI.java
@@ -54,7 +54,7 @@ public class SimulationGUI {
 
 		frame = new JFrame("Simulation");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(790, 830);
+		frame.setSize(790, 835);
 		frame.setLocationRelativeTo(null);
 		
 		GridBagConstraints c = new GridBagConstraints();
